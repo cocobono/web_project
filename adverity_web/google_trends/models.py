e5df834d84a94027c0f97b8db9767a805bf1b3ec
cocobono <ccontawe@gmail.com>
2018-08-19 21:16:59 +0200

    added notebooks and models

--- adverity_web/google_trends/models.py
+++ adverity_web/google_trends/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    signup_stamp = models.DateTimeField(auto_now=True)
+
+
+class Request(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now=True)
+    search_term = models.CharField(max_length=100)
+
+
+class TimeStamp(models.Model):
+    request = models.ForeignKey("Request", on_delete=models.CASCADE)
+    value = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now=True)
